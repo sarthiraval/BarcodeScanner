85db6f78614b07aeafcafe2976548bea45739e34
Sarthi <ssr20810@gmail.com>
2023-04-22 18:20:11 -0700

    first commit

---
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -0,0 +1,176 @@
+package com.example.barcodescanner;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.zxing.WriterException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView qrCodeIV;
+    private EditText dataEdt;
+    private Button generateQrBtn,b2;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        qrCodeIV = findViewById(R.id.idIVQrcode);
+        dataEdt = findViewById(R.id.idEdt);
+        generateQrBtn = findViewById(R.id.idBtnGenerateQR);
+        b2 = findViewById(R.id.done);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout savingLayout = (LinearLayout) findViewById(R.id.newss);
+                File file = saveBitMap(MainActivity.this, savingLayout);
+                if (file != null) {
+                        Log.i("TAG", "Drawing saved to the gallery!");
+                    dataEdt.getText().clear();
+                    qrCodeIV.setImageDrawable(null);
+
+                } else {
+                    Log.i("TAG", "Oops! Image could not be saved.");
+                }            }
+
+        });
+
+        generateQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(dataEdt.getText().toString())) {
+
+                    Toast.makeText(MainActivity.this, "Enter some text to generate QR Code", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+                    Display display = manager.getDefaultDisplay();
+
+                    Point point = new Point();
+                    display.getSize(point);
+
+                    int width = point.x;
+                    int height = point.y;
+                    int dimen = width < height ? width : height;
+                    dimen = dimen * 3 / 4;
+
+                    qrgEncoder = new QRGEncoder(dataEdt.getText().toString(), null, QRGContents.Type.TEXT, dimen);
+                    try {
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        qrCodeIV.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        Log.e("Tag", e.toString());
+                    }
+
+                }
+            }
+        });
+    }
+
+    private File saveBitMap(Context context, View drawView) {
+        File pictureFileDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Scanner 2");
+        if (!pictureFileDir.exists()) {
+            boolean isDirectoryCreated = pictureFileDir.mkdirs();
+            if (!isDirectoryCreated)
+                Log.i("TAG", "Can't create directory to save the image");
+            return null;
+        }
+        String filename = pictureFileDir.getPath() + File.separator + System.currentTimeMillis() + ".jpg";
+        File pictureFile = new File(filename);
+        Bitmap bitmap = getBitmapFromView(drawView);
+        try {
+            pictureFile.createNewFile();
+            FileOutputStream oStream = new FileOutputStream(pictureFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, oStream);
+            oStream.flush();
+            oStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.i("TAG", "There was an issue saving the image.");
+        }
+        scanGallery(context, pictureFile.getAbsolutePath());
+        return pictureFile;
+    }
+    private Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas);
+        } else {
+            canvas.drawColor(Color.WHITE);
+        }
+        view.draw(canvas);
+        return returnedBitmap;
+    }
+
+
+    private void scanGallery(Context cntx, String path) {
+        try {
+            MediaScannerConnection.scanFile(cntx, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                public void onScanCompleted(String path, Uri uri) {
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("TAG", "There was an issue scanning gallery.");
+        }
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
+}
